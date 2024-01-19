7b51e56433ecad5dffed450e74063831f77dd200
gayatrig19 <>
2024-01-19 19:23:46 +0000

    Update views for hello_world app

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello, world!")
+
 
 def index(request):
     if request.method == "POST":
